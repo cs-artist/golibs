380640e618547d0acb1005893d8d560adddc23a6
bennyzhu <bennyzhu@tencent.com>
2023-08-06 10:57:57 +0800

    redis locker

---
+++ frame/go.mod
@@ -0,0 +1,13 @@
+module frame
+
+go 1.18
+
+require (
+	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/google/uuid v1.3.0
+)
+
+require (
+	github.com/onsi/ginkgo v1.16.5 // indirect
+	github.com/onsi/gomega v1.27.10 // indirect
+)
---
+++ frame/locker/redis_locker.go
@@ -0,0 +1,54 @@
+// redis locker: https://mp.weixin.qq.com/s/Uo507elzCXzI0eI7mDHpGg
+package locker
+
+import (
+	"errors"
+	"time"
+
+	"github.com/go-redis/redis"
+	"github.com/google/uuid"
+)
+
+var client = redis.NewClient(&redis.Options{
+	Addr:     "localhost:6379",
+	Password: "",
+	DB:       0,
+})
+
+// lock: value为能识别该routine的唯一值（如uuid，orderid等）
+func Lock(key, value string, expiration time.Duration) error {
+	ok, err := client.SetNX(key, value, expiration).Result()
+	if err != nil {
+		return err
+	}
+	if !ok {
+		return errors.New("key exist")
+	}
+	return nil
+}
+
+// unlock：使用lua脚本保障操作的原子性
+func Unlock(key, value string) error {
+	var luaScript = redis.NewScript(`
+		if redis.call("get", KEYS[1]) == ARGV[1]   // compare value, if equal then del
+		then
+			return redis.call("del", KEYS[1])
+		else
+			return -1
+		end
+	`)
+	ret, err := luaScript.Run(client, []string{key}, value).Int()
+	if err != nil {
+		return err
+	}
+	if ret < 0 {
+		return errors.New("value not equal")
+	} else if ret == 0 {
+		return errors.New("del fail")
+	}
+	return nil
+}
+
+func UUID() string {
+	return uuid.NewString()
+}
